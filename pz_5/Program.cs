809d57cb3c343b157b28e10d5ecdd24302279d2d
Влада Суханова <suhanova.vlada.29.03@gmail.com>
2022-09-22 18:53:11 +0500

    test edit

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Задание_1");
+            int i = 0;
         }
     }
 }
