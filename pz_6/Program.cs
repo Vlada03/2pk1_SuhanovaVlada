10620f39287fce0b4d7ef07d14a9005dedfe5ba7
Влада Суханова <suhanova.vlada.29.03@gmail.com>
2022-09-26 19:53:38 +0500

    test edit

---
+++ pz_6/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задана функция y = |x|? где -4 <= x <= 4. Шаг: 0,5");
+            double x = -4;
+            Console.WriteLine("y = ");
+            do
+            {
+                Console.WriteLine(Math.Abs(x));
+                x += 0.5;
+            }
+            while (x <= 4);
+        }
+    }
+}
