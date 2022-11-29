822ae32807eaaf40880f4a13741c5209798d9241
Влада Суханова <suhanova.vlada.29.03@gmail.com>
2022-11-29 22:52:06 +0500

    text edit

---
+++ pz_15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace pz_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите путь к папке: ");
+            string dirName = Console.ReadLine();
+            String[] files = Directory.GetFiles(dirName); //поменять на директори
+            if (files.Length == 0)
+                Console.WriteLine("Файлов нет");
+            else if (new DirectoryInfo(dirName).GetFiles("*.exe").Any(x => x.Extension == ".exe"))
+            {
+                Console.WriteLine("Файлы с расширением .exe");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
---
+++ pz_8/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace pz_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[3]; 
+
+        }
+    }
+}
